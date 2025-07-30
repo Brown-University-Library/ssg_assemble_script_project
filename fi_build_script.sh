29f4e4b76f609de096810ce2b052a473c77bbe93
Birkin James Diana <birkin.diana@gmail.com>
2025-07-30 09:10:09 -0400

    adds realpaths and improves echos.

--- fi_build_script.sh
+++ fi_build_script.sh
@@ -1,10 +1,21 @@
-## asumptions:
-## - code is run from the `fi_build_script` directory
+## build-script for assembling the stand-alone, non-server FI static-site
+echo " "
+echo ":: starting fi_build_script.sh"
+echo " "
+
+## confirm we're in the correct `fi_build_script` directory ---------
+script_dir="$(realpath "$(dirname "$0")")" || { echo "Error: Failed to get realpath of directory $(dirname "$0")" >&2; exit 1; }
+cd "$script_dir" || { echo "Error: Failed to change to directory $script_dir" >&2; exit 1; }
+echo ":: confirmed we're staring from the correct directory: $script_dir"
+echo " "
 
 ## source the dotenv file -------------------------------------------
-source ../fi_build_dotenv.sh || { echo "Error: Failed to source fi_build_dotenv.sh" >&2; exit 1; }
+dotenv_path="$(realpath "../fi_build_dotenv.sh")" || { echo "Error: Failed to get realpath of fi_build_dotenv.sh" >&2; exit 1; }
+source "$dotenv_path" || { echo "Error: Failed to source $dotenv_path" >&2; exit 1; }
+echo ":: envars sourced from: $dotenv_path"
+echo " "
 
-## set vars
+## set vars ---------------------------------------------------------
 LOCAL_BUILD_DIR="$(realpath "../fashioning_insurrection_site_build")"
 LOCAL_IMAGE_DIR="$(realpath "../fashioning_insurrection_site_build/img")"
 LOCAL_FONT_DIR="$(realpath "../fashioning_insurrection_site_build/fonts")"
@@ -12,8 +23,7 @@ GIT_BUILD_REPO="https://github.com/Brown-University-Library/fashioning_insurrect
 REMOTE_SERVER=$FI_BUILD__REMOTE_SERVER          # from the dotenv-source
 REMOTE_IMAGE_DIR=$FI_BUILD__REMOTE_IMAGE_DIR    # from the dotenv-source
 REMOTE_FONT_DIR=$FI_BUILD__REMOTE_FONT_DIR      # from the dotenv-source
-
-echo "vars prepared..."
+echo ":: vars prepared..."
 echo "- LOCAL_BUILD_DIR: $LOCAL_BUILD_DIR"
 echo "- LOCAL_IMAGE_DIR: $LOCAL_IMAGE_DIR"
 echo "- LOCAL_FONT_DIR: $LOCAL_FONT_DIR"
@@ -30,3 +40,6 @@ git pull $GIT_BUILD_REPO
 cd "$LOCAL_BUILD_DIR" || { echo "Error: Failed to change to directory $LOCAL_BUILD_DIR" >&2; exit 1; }
 rsync -avz --delete "$LOCAL_IMAGE_DIR" "$REMOTE_SERVER:$REMOTE_IMAGE_DIR"
 rsync -avz --delete "$LOCAL_FONT_DIR" "$REMOTE_SERVER:$REMOTE_FONT_DIR"
+
+cd "$script_dir" || { echo "Error: Failed to change to directory $script_dir" >&2; exit 1; }
+echo ":: fi_build_script.sh completed successfully"
